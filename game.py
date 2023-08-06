dc6139fa9985b53fcd12d695a0352c819900e7b7
Niranjan Shah <niranjanshah474@gmail.com>
2023-08-07 00:59:22 +1000

    horizontal move timer added and user input added

--- game.py
+++ game.py
@@ -25,7 +25,8 @@ class Game:
 
         #timer
         self.timers = {
-            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move': Timer(MOVE_WAIT_TIME)
         }
         self.timers['vertical move'].activate()
 
@@ -50,6 +51,7 @@ class Game:
     def run(self):
 
         #update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -62,8 +64,16 @@ class Game:
         self.display_surface.blit(self.surface, (PADDING,PADDING))
         pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
 
+    def input(self):
+        keys = pygame.key.get_pressed()
 
-
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
 class Tetromino:
     '''
     Tetromino is the combination of 4 blocks
@@ -78,6 +88,10 @@ class Tetromino:
 
         # create blocks
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
 
     def move_down(self):
         for block in self.blocks:
